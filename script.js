460479953b60186d3276c146484692528f7a3da2
sahilkankotiya <kankotiyasahil@gmail.com>
2025-06-21 09:16:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  str = str.trimStart();
+  const spaceIndex = str.indexOf(' ');
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  return str.substring(0, spaceIndex);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
